faa3342b9a31a6074fa233ecb2f14280fba5449c
Aswin Muthu Kumar <ashelangovan97@gmail.com>
2019-08-19 07:31:00 +0530

    Updated node_modules
    Signed-off-by: Aswin Muthu Kumar <ashelangovan97@gmail.com>

---
+++ node_modules/botbuilder/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @module botbuilder
+ */
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export * from './botFrameworkAdapter';
+export * from './fileTranscriptStore';
+export * from './inspectionMiddleware';
+export * from 'botbuilder-core';
